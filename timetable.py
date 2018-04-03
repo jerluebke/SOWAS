3adb8d73959215a82fc207856909b057d4a0d267
Jeremiah Lübke <jer.luebke@web.de>
2018-04-03 15:47:40 +0200

    added eps files, finished first draft of exposee with latex and timetable graphic

--- timetable.py
+++ timetable.py
@@ -1,28 +1,32 @@
-#coding=utf-8
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable = C, R
 
 from collections import namedtuple
 
 import matplotlib as mpl
 from matplotlib.patches import Patch
+from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 import numpy as np
 
-mpl.rcParams["text.latex.unicode"] = True
+mpl.rcParams["font.size"] = 12
 
 
+# colors
 cmap = plt.get_cmap("Set1")
 color_mapping = {
-                    "th" : cmap(0),     # theory
-                    "ex" : cmap(1),     # experiment
-                    "ev" : cmap(2)      # evaluation
-                }
+    "th" : cmap(0),     # theory
+    "ex" : cmap(1),     # experiment
+    "ev" : cmap(2)      # evaluation
+    }
 
 
 class item(namedtuple("item",
                       ["description", "start", "duration", "domain"])):
     """
     item in the timetable containing startpoint (starting with 0), duration,
-    domain (theory, experiment, evaluation) and description (as it appears in 
+    domain (theory, experiment, evaluation) and description (as it appears in
     the plot)
     """
     __slots__ = ()
@@ -35,21 +39,18 @@ class item(namedtuple("item",
 #   data    #
 #############
 
-items = [   
-            item("Einarbeitung in Python", 0, 2, "th"),
-            item("Erstellen der Simulation", 1, 4, "th"),
-            item("Ergänzung der Simulation", 5, 2, "th"),
-            item("Bau der Messvorrichtung", 0, 2, "ex"),
-            item("Aufnahme der Messreihen", 2, 4, "ex"),
-            item("Anpassung des Aufbaus", 6, 3, "ex"),
-            item("Auswertung/Erstellung des Posters", 8, 3, "ev")
-        ]
+items = [
+    # item: description, start, duration, domain
+    item("Einarbeitung\n in Python", 0, 2, "th"),
+    item("Erstellen der\n Simulation", 1, 4, "th"),
+    item("Ergänzung der\n Simulation", 5, 2, "th"),
+    item("Bau der Mess-\n vorrichtung", 0, 2, "ex"),
+    item("Aufnahme der\n Messreihen", 2, 4, "ex"),
+    item("Anpassung des\n Aufbaus", 6, 3, "ex"),
+    item("Auswertung/\nErstellung des\n Posters", 8, 3, "ev")
+    ]
 
-data_as_array = np.array(
-                            [[  item.start,
-                                item.duration ]
-                            for item in items ]
-                        )
+data_as_array = np.array([[item.start, item.duration] for item in items])
 
 y_values        = np.arange(len(items))
 starting_points = data_as_array[:,0]
@@ -58,16 +59,21 @@ y_labels        = [item.description for item in items]
 colors          = [item.color for item in items]
 
 kwargs = {
-            "height" : .5, 
-            "align" : "center"
-         }
+    "height"    :   .4,
+    "align"     :   "center"
+    }
 
 
 #################
 #   plotting    #
 #################
 
-fig, ax = plt.subplots()
+fig = plt.figure(figsize=(8, 6))
+# make two subplots - actual plot and legend
+gs = GridSpec(2, 1, height_ratios=[11, 1])
+
+# make timetable
+ax = plt.subplot(gs[0])
 ax.barh(y_values, durations, left=starting_points, color=colors, **kwargs)
 
 # adjust yaxis
@@ -77,18 +83,28 @@ ax.set_yticklabels(y_labels)
 # hide yticks
 ax.tick_params(axis='y', length=0)
 
-# set xticks, label and grid
+# xaxis: set ticks, label and limits
 ax.set_xticks(np.arange(12))
 ax.set_xlabel("Wochen (09.04.2018 - 22.06.2018)")
 ax.set_xlim((0, 11))
+# turn grid off to avoid conflicts with local settings
+ax.grid(False)
 # place grid below elements in plot
 ax.set_axisbelow(True)
 ax.grid(axis='x')
 
-plt.title("Zeitplan")
+# make legend in second subplot
+legend_ax = plt.subplot(gs[1])
+# remove ticks and boundary box
+legend_ax.set(xticks=[], yticks=[])
+legend_ax.set_axis_off()
 # make and map proxy artists to legend
 plt.legend(handles=[Patch(color=c, label=l)
-                    for c, l in zip(color_mapping.values(), 
-                                    ("Theorie", "Experiment", "Auswertung"))
-                   ]
-          )
+                    for c, l in zip(color_mapping.values(),
+                                    ("Theorie", "Experiment", "Auswertung"))],
+           loc="lower center", ncol=3)
+
+# allign subplots
+plt.tight_layout()
+
+plt.savefig("timetable.eps", format="eps", dpi=1000)
