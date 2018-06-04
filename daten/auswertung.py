1422fc448dfcc5874f384377e60f4b7b0799258d
Jeremiah Lübke <jer.luebke@web.de>
2018-06-04 20:37:35 +0200

    added evaluation and data samples

---
+++ daten/auswertung.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("..")
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import DominoChain as dc
+import dominoes
+
+
+#################
+#   GLOBALS     #
+#################
+
+# general settings
+DATA_DIR        = "./messreihe02/text"
+FILE_TYPE       = ".txt"
+FIRST_ELEMS     = 4
+LAST_ELEMS      = 1
+FIGSIZE         = (12, 8)
+ERRORBAR_PROPS  = dict(ecolor="black", capsize=6, marker="o", linestyle="None")
+DPI             = 300
+
+# spacings and friction
+LAMBDAS = np.array([
+    1.5,
+    2.0,
+    2.5,
+    3.0,
+    3.5,
+    4.0,
+])
+LAMBDAS_LINSPACE = np.linspace(1.5, 4)
+MU = [
+    0.2,
+    #  0.3,
+    #  0.4,
+    #  0.5,
+    #  0.6,
+    #  0.7
+]
+
+# saving figures here
+FIGURE  = []
+
+# theoretical utility
+DOMINO_CHAIN = dc.PyDominoChain(dc.domino_tuple(4.2, 0.6))
+
+
+#################
+#   FUNCTIONS   #
+#################
+
+def get_data()->np.ndarray:
+    return np.array([get_content(file) for file in os.listdir(DATA_DIR)
+                     if file.endswith(FILE_TYPE)])
+
+def get_content(file:str)->np.ndarray:
+    with open(os.path.join(DATA_DIR, file), 'r') as f:
+        lines = f.readlines()
+    return np.array(lines[FIRST_ELEMS:], dtype=np.float64)
+
+def calc_mean_and_std(data:np.ndarray)->np.ndarray:
+    return np.array([
+        (lambda arr: [np.mean(arr), np.std(arr)])(np.abs(elem[:-1] - elem[1:]))
+        for elem in data
+    ])
+
+def make_plot(xdata, ydata, yerr)->list:
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+    ax.errorbar(xdata, ydata, yerr,
+                label="Experimentelle Daten",
+                **ERRORBAR_PROPS)
+    return [fig, ax]
+
+
+def main():
+    global FIGURE, AXIS
+
+    # prepare data
+    raw_data = get_data()*10
+    time_mean, time_err = calc_mean_and_std(raw_data).transpose()
+    ydata = LAMBDAS / time_mean
+    yerr = LAMBDAS * time_err / time_mean**2
+
+    # compare with theoretical intrinsic transversal
+    FIGURE.append(make_plot(LAMBDAS, ydata, yerr)[0])
+    #  ax_1 = FIGURE[0].get_axes()[0]
+    #  [ax_1.plot(LAMBDAS_LINSPACE, (lambda x: DOMINO_CHAIN.intrinsic_velocities(
+    #      x-0.3, mu)[1])(LAMBDAS_LINSPACE), label='$\mu$ = {:.1f}'.format(mu)) for mu in MU]
+
+    dominoes.main()
+    #  dominoes.WITH_FRICTION = True
+    #  dominoes.main()
+    #  plt.plot(LAMBDAS_LINSPACE, 4*dominoes.RESULT["y"])
+
+    #  for fig in FIGURE:
+    #      fig.legend()
+    #      fig.savefig("%d.png" % fig.number, dpi=DPI)
+
+    ax_01 = FIGURE[0].get_axes()[0]
+    ax_01.set(
+        title="Vergleich: Experimentelle und Theoretische Daten",
+        xlabel="Abstand in m",
+        ylabel="V in m/s"
+    )
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
--- daten/dominoes.py
+++ daten/dominoes.py
@@ -9,38 +9,37 @@ import matplotlib.pyplot as plt
 #  GLOBALS  #
 #############
 
-L               = 0
-H               = 0
-PIECES          = 0
-PHI             = 0
-OMEGA_SQUARED   = 0
-LAMBDA          = 0
-PSI             = 0
-D_THETA         = 0
-ETA             = 0     # = (λ + h) / L
-THETA_HAT       = 0     # = arccos(h / (h + λ))
-PHI_DOT         = 0
+L       = 0
+H       = 0
+PIECES  = 0
+PHI     = 0
+OMEGA_SQUARED = 0
 
-MU              = 0.2   # coefficient of friction
-INT_STEPS       = 50    # number of integration steps
-WITH_FRICTION   = False
+LAMBDA      = 0
+PSI         = 0
+D_THETA     = 0
+ETA         = 0
+THETA_HAT   = 0
+PHI_DOT     = 0
+
+MU = 0.2
+REL_STEPS = 50
+WITH_FRICTION = True
 
 G = 9.80665
 
 FIGURE = None
-RESULT = []
+RESULT = {}
 
 
 ###############
 #  FUNCTIONS  #
 ###############
 
-def velocity(lambda_value):
-    update(lambda_value)
+def velocity():
     return (LAMBDA + H) / time()
 
 def time():
-    # Integration of 1/theta_dot
     return np.sum(D_THETA / theta_dot(np.arange(0, PSI, D_THETA)))
 
 def theta_dot(thetas : np.ndarray) -> np.ndarray:
@@ -54,7 +53,7 @@ def k(theta):
                        for j in range(PIECES)])
 
 def theta_dot_rel(index, theta_initial) -> np.ndarray:
-    thetas = np.empty(index + 1)
+    thetas = np.zeros(index + 1)
     thetas[0] = theta_initial
     for i in range(1, thetas.size):
         thetas[i] = np.arcsin(ETA * np.cos(thetas[i-1]) - H/L) + thetas[i-1]
@@ -65,52 +64,50 @@ def P_over_K(thetas : np.ndarray) -> np.ndarray:
 
 def phi_dot():
     k_value = k(0)
-
     friction_term = 1
     if WITH_FRICTION:
         xi = L * np.cos(PSI)
         R = 1 + (xi + MU * LAMBDA) / (xi - MU * H)
         friction_term = k_value * R**2 - k_value + 1
-
     return np.sqrt(OMEGA_SQUARED * (k_value-1) / k_value
                    * 2 * (np.cos(PHI) - np.cos(THETA_HAT - PHI))
                    / friction_term)
 
 
-###########
-#  SETUP  #
-###########
-
 def init(height, width, pieces=6):
     global L, H, PIECES, PHI, OMEGA_SQUARED
-    L               = height
-    H               = width
-    PIECES          = pieces
-    PHI             = np.arctan(H / L)
-    OMEGA_SQUARED   = 3 * G * np.cos(PHI) / (2 * L)
+    L = height
+    H = width
+    PIECES = pieces
+    PHI = np.arctan(H / L)
+    OMEGA_SQUARED = 3 * G * np.cos(PHI) / (2 * L)
 
 def update(spacing):
     global LAMBDA, PSI, D_THETA, ETA, THETA_HAT, PHI_DOT
-    LAMBDA      = spacing
-    PSI         = np.arcsin(LAMBDA / L)
-    D_THETA     = PSI / INT_STEPS
-    ETA         = (LAMBDA + H) / L
-    THETA_HAT   = np.arccos(H / (H + LAMBDA))
-    PHI_DOT     = phi_dot()
+    LAMBDA = spacing
+    PSI = np.arcsin(LAMBDA / L)
+    D_THETA = PSI / REL_STEPS
+    ETA = (LAMBDA + H) / L
+    THETA_HAT = np.arccos(H / (H + LAMBDA))
+    PHI_DOT = phi_dot()
 
 
 def main():
     global FIGURE
 
     init(4.2, 0.5)
-    spacings    = np.linspace(1.5, 4)
-    velocities  = np.array(map(velocity, spacings))
-    RESULT.append(dict(x=spacings, y=velocities))
+    spacings = np.linspace(1.5, 4)
+    velocities = np.zeros(spacings.shape)
+    for i in range(spacings.size):
+        update(spacings[i])
+        velocities[i] = velocity()
+
+    RESULT["x"] = spacings
+    RESULT["y"] = velocities
 
     if not FIGURE:
-        FIGURE = plt.gcf()
-    plt.plot(spacings, velocities, label="Stronge & Shu '88")
-    plt.legend()
+        FIGURE = plt.figure()
+    plt.plot(spacings, velocities)
 
 
 if __name__ == "__main__":
