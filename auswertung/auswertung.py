9c56717b6f802b104a2fd9ed2d8433f5127724be
Jeremiah Lübke <jer.luebke@web.de>
2018-06-16 00:53:56 +0200

    .

--- auswertung/auswertung.py
+++ auswertung/auswertung.py
@@ -3,31 +3,28 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
+import matplotlib as mpl
 import dominoes
 import v_vs_x
+
+mpl.rcParams["font.size"] = 22
 
 
 EXP_DATA_ALL    = "./velocities_by_x.dat"
 EXP_DATA_3CM    = "./experimental_data_avg.dat"
-FIGSIZE         = (32, 16)   #  (12, 8)
+FIGSIZE         = (16, 8)   #  (12, 8)
 ERRORBAR_PROPS  = dict(ecolor="black", capsize=5, marker="o",
-                       markersize=6, color="red",
+                       markersize=8, color="red",
                        linestyle="None")
 DPI             = 300
 DOMINO_PROPS    = dict(height=4.2, width=0.6, energy_loss=0.9)
 PIECES          = 30
 INITIAL_VEL     = 0.8
-LABEL_EXP       = "Experimentelle Daten"
-LABEL_TH        = "Theoretischer Verlauf - $\mu$ = %.1f" % 0.1
-
-LEGEND = None
-LEGEND_PROPS = dict(loc="upper center"
-                    # , bbox_to_anchor=(.5, 0)
-                   )
+LABEL_EXP       = "Experiment"
+LABEL_TH        = "Theorie"
 
 
-def main_compare():
+def main_compare(save=False):
     # init calculations
     v_vs_x.init(**DOMINO_PROPS, spacing=2.4, length=PIECES, initial=INITIAL_VEL)
     dominoes.init(**DOMINO_PROPS)
@@ -38,26 +35,36 @@ def main_compare():
     spacings    = np.linspace(1.5, 4)
     vel_all     = np.array(list(map(lambda x: dominoes.velocity(x),
                                     spacings-0.6)))
+    vel_as      = dominoes.velocity(2.4)
 
     # plotting
     fig, ax = plt.subplots(ncols=2, figsize=FIGSIZE)
 
     # progression of velocity for 3 cm spacing
-    exp = ax[0].errorbar(pos, vel_3cm_exp.mean(axis=1), vel_3cm_exp.std(axis=1),
-                         **ERRORBAR_PROPS)
-    ax[0].plot(pos, vel_3cm_th)
+    ax[0].errorbar(pos, vel_3cm_exp.mean(axis=1), vel_3cm_exp.std(axis=1),
+                   **ERRORBAR_PROPS, label=LABEL_EXP)
+    ax[0].plot(pos, vel_3cm_th,
+               label=LABEL_TH)
     # add horizontal line indicating the asymptotical velocity
-    ax[0].plot((0, 70), 2*(dominoes.velocity(2.4),), "--", color="grey")
+    ax[0].plot((0, 70), 2*(vel_as,), "--", color="grey")
+    # adjustments
+    ax[0].set_xlim(1.5, 61.5)
 
     # plot velocity vs spacing
     ax[1].errorbar(exp_data[::,0], exp_data[::,1], exp_data[::,2],
                    **ERRORBAR_PROPS)
     ax[1].plot(spacings, vel_all)
 
-    # adjustments
-    ax[0].set_xlim(1.5, 61.5)
+    # text-related
+    ax[0].legend(loc="lower right", numpoints=2)
+    ax[0].set(xlabel="x [$10^{-2}$ m]",
+              ylabel="v [$\\frac{m}{s}$]",
+              title="Geschwindigkeitsverlauf für d = 3 cm")
+    ax[1].set(xlabel="d [$10^{-2}$ m]",
+              ylabel="v [$\\frac{m}{s}$]",
+              title="$v_{as}$ verschiedener Abstände")
 
-    th_line = mlines.Line2D([], [], color="blue")
-    global LEGEND
-    LEGEND = fig.legend((exp, th_line), (LABEL_EXP, LABEL_TH))
+    if save:
+        plt.tight_layout()
+        plt.savefig("%s.png" % input("name: "), dpi=DPI)
 
